8fde116d6058358aedf2c9c7e8b19ff854bd2580
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-19 22:52:54 -0300

    feat: Calculo do ticket médio

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ pd.set_option('display.max_columns', None) #=> Mostra todas as colunas no termin
 # Faturamento por loja
 faturamento = tab_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
+print('-' * 50)
+
 # Quantidade de produtos vendidos por loja
+qtd_produtos_vendidos = tab_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(qtd_produtos_vendidos)
+print('-' * 50)
 
 # Ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / qtd_produtos_vendidos['Quantidade']).to_frame()
+print(ticket_medio)
 
 # Enviar email com relatório
