910ff3077f5de539e4e2391c3b1dfaf0ce74045b
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-19 22:39:25 -0300

    feat: Calcula faturamento das lojas

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ tab_vendas = pd.read_excel('Vendas.xlsx')
 
 # Visualizar a base de dados
 pd.set_option('display.max_columns', None) #=> Mostra todas as colunas no terminal
-print(tab_vendas)
 
 # Faturamento por loja
-
+faturamento = tab_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
 # Quantidade de produtos vendidos por loja
 
 # Ticket médio por produto em cada loja
