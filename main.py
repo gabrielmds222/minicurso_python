effaa5f15d1a3ef2648944499f16bf7e6f7c9771
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-19 22:11:30 -0300

    Iniciando minicurso

---
+++ main.py
@@ -0,0 +1,6 @@
+# Importar a base de dados
+# Visualizar a base de dados
+# Faturamento por loja
+# Quantidade de produtos vendidos por loja
+# Ticket médio por produto em cada loja
+# Enviar email com relatório
