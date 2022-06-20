468741ac8b212309c81db38e0ec2050dd4c699fb
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-19 22:24:24 -0300

    feat: Visualiza base de dados no terminal

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Importar a base de dados
 import pandas as pd
+
 # Visualizar a base de dados
+tab_vendas = pd.read_excel('Vendas.xlsx')
+
+print(tab_vendas)
 # Faturamento por loja
+
 # Quantidade de produtos vendidos por loja
+
 # Ticket médio por produto em cada loja
+
 # Enviar email com relatório
